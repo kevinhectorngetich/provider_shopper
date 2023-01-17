af9b8bca73ab6f19d19d8d5f1d6ba4c52a2c0ad7
KevinHector <ngetichkevinhector@gmail.com>
2023-01-17 07:00:27 +0300

    version 0.0.1

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_shopper/models/cart.dart';
 
